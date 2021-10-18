da3d74cb970ddd9254a28871ad9550e2d4fbe67b
Timofii <timoxafos@gmail.com>
2021-10-18 19:01:32 +0300

    fix arrow_func, pow, class

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function recursioneBrackets(num) {
   if (num < 1) {
     return "()";
   }
-  num = recursioneBrackets(num - 1, right = "", left = "");
+  num = recursioneBrackets(num - 1, (right = ""), (left = ""));
   right += ")";
   left += "(";
 
@@ -11,13 +11,14 @@ function recursioneBrackets(num) {
 }
 console.log(recursioneBrackets(5));
 
-
 let arrs = [1, 2, 3, 4, 5, 6, 7];
 let max = Math.max(...arrs);
 let min = Math.min(...arrs);
 console.log(max, min);
 
-const arrowFuncSum = (...arg) => arg.reduce((acc, el) => acc + el, 0);
+const arrowFuncSum = (...arg) => arg.reduce((acc, el) => acc * el, 1);
+
+console.log(arrowFuncSum(1, 2, 3, 4, 5));
 
 function recursionePow(num, exp) {
   if (exp === 0) {
@@ -26,63 +27,57 @@ function recursionePow(num, exp) {
   if (exp < 0) {
     return recursionePow(num, exp + 1) / num;
   }
+  return num * recursionePow(num, exp - 1);
 }
 
 console.log(recursionePow(6, -6));
 
 // подчет гласных №2
 function wrapper(str) {
-  return str.toLowerCase().split('')
+  return str.toLowerCase().split("");
 }
 
-let str = "Зима близко Джон Сноу"
+let str = "Зима близко Джон Сноу";
 const getVowels = (str) => {
-  let vowels = wrapper('аеуио')
+  let vowels = wrapper("аеуио");
   let result = wrapper(str).reduce((acc, el) => {
     vowels.includes(el) ? acc++ : 0;
     return acc;
   }, 0);
-  return result
+  return result;
 };
 
 console.log(getVowels(str));
 
-
-
 // классы №3
 
 class RangeValidator {
-    constructor(from, to) {
-      this.from = from;
-      this.to = to;
-      this.validVal = (val) => {
-        if (typeof val !== "number" || val < 0) throw new Error("Must be a number > 0");
-      };
-    }
-    set setFrom(val) {
-      this.from = this.validVal(val);
-    }
-    get getFrom() {
-      return this.from;
-    }
-    set setTo(val) {
-      this.to = this.validVal(val);
-    }
-    get getTo() {
-      return this.to;
-    }
-    get getRange() {
-      return Array(this.from, this.to);
-    }
-    validate(val) {
-      this.validVal(val);
-      if (val >= this.from && val <= this.to) {
-        return val;
-      } else {
-        throw new TypeError("Your number is out of range, try againe");
-      }
+  constructor(from, to) {
+    this.from = from;
+    this.to = to;
+  }
+  set setFrom(val) {
+    this.from = this.validate(val);
+  }
+  get getFrom() {
+    return this.from;
+  }
+  set setTo(val) {
+    this.to = this.validate(val);
+  }
+  get getTo() {
+    return this.to;
+  }
+  get getRange() {
+    return Array(this.from, this.to);
+  }
+  validate(val) {
+    if (val >= this.from && val <= this.to) {
+      return val;
+    } else {
+      throw new TypeError("Your number is out of range, try againe");
     }
   }
-  
-  const range = new RangeValidator(RANGE_FROM, RANGE_TO);
-  
+}
+
+const range = new RangeValidator(RANGE_FROM, RANGE_TO);
