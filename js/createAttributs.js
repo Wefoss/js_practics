4c0e39a2598c61dea32806f9cf87331b4b84dad8
Timofii <timoxafos@gmail.com>
2021-10-31 16:07:28 +0200

    createAttributs

---
+++ js/createAttributs.js
@@ -0,0 +1,19 @@
+const arrayImages = [
+    'https://www.theuiaa.org/wp-content/uploads/2017/11/RTM19-banner-web.jpg',
+    'https://files.worldwildlife.org/wwfcmsprod/images/Mountains_New_Hero_Image/hero_small/1bs3lrclhi_mountains_hero.jpg'
+  ]
+
+const img = document.querySelector('img')
+const [prev, next] = document.querySelectorAll('button')
+
+let item = document.createAttribute("src");
+
+const foo = () =>  {
+  let i = arrayImages.indexOf(img.getAttribute("src")) === 0 ? 1 : 0;
+  item.value = arrayImages[i];
+  img.setAttributeNode(item);
+};
+
+foo()
+prev.addEventListener("click", foo);
+next.addEventListener("click", foo);
